3d2af7dd2438f948fba19843ead2ce23bb0204ca
Divyanshi Agrawal <divsagr3@gmail.com>
2020-10-15 14:51:25 +0530

    Add image to home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 function Home() {
-    return (
-        <div className="home">
-            <h1>Home page</h1>
-        </div>
-    )
+  return (
+    <div className="home">
+      <h1>Home page</h1>
+      <img src="https://cdn.pixabay.com/photo/2020/06/29/17/26/photographer-5353515_1280.jpg" />
+    </div>
+  );
 }
 
-export default Home
+export default Home;

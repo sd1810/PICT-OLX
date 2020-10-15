faf7dd252d2361efd874b85810d0f6c2027f4c2f
Divyanshi Agrawal <divsagr3@gmail.com>
2020-10-15 15:45:06 +0530

    Delete home image

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,6 @@ function Home() {
   return (
     <div className="home">
       <h1>Home page</h1>
-      <img src="https://cdn.pixabay.com/photo/2020/06/29/17/26/photographer-5353515_1280.jpg" />
     </div>
   );
 }
